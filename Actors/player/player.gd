132a06dbea059fbe4439f0e4964f3e3cad85f7d8
Iranitas <iranitasinc@gmail.com>
2025-05-27 01:55:06 -0500

    Added running animation and animation transition too/from idle.

--- Actors/player/player.gd
+++ Actors/player/player.gd
@@ -13,6 +13,7 @@ func _physics_process(delta):
 
 	# Handle jumpies
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#$AnimatedSprite2D.play("Jump")
 		velocity.y = JUMP_VELOCITY
 		
 	# Pressing 'G' lets you manually trigger things for testing
@@ -22,17 +23,24 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("attack"):
 		print("attacked!")
 		$AnimatedSprite2D.play("Attack")
+		return
+		
+		
 	# Get the input direction and handle the movement/deceleration.
 	# prolly replace UI default actions with our own custom ones..
+	# add new animation sprite thingy? must research.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		if(direction > 0):
-			$AnimatedSprite2D.flip_h = false
-		elif (direction < 0):
-			$AnimatedSprite2D.flip_h = true
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = direction < 0
+		
+		if(direction != 0) and is_on_floor() and $AnimatedSprite2D.animation != "Run":
+			$AnimatedSprite2D.play("Run")
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if (direction == 0) and is_on_floor() and $AnimatedSprite2D.animation != "Idle" and $AnimatedSprite2D.animation != "Attack":
+			$AnimatedSprite2D.play("Idle")
 
 	move_and_slide()
 
